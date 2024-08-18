6334d2c05aed75662605b535663c632b3896f61c
jerolba <jerolba@gmail.com>
2024-08-18 14:03:43 +0000

    Join CarpetWriterSupport with CarpetMessageWriter

--- carpet-record/src/main/java/com/jerolba/carpet/impl/write/CarpetWriterSupport.java
+++ carpet-record/src/main/java/com/jerolba/carpet/impl/write/CarpetWriterSupport.java
@@ -61,4 +61,24 @@ public class CarpetWriterSupport<T> extends WriteSupport<T> {
     public void write(T record) {
         carpetWriter.write(record);
     }
+
+    private static class CarpetMessageWriter<T> {
+
+        private final RecordConsumer recordConsumer;
+        private final CarpetRecordWriter writer;
+
+        CarpetMessageWriter(RecordConsumer recordConsumer, Class<T> recordClass,
+                CarpetWriteConfiguration carpetConfiguration) {
+            this.recordConsumer = recordConsumer;
+            this.writer = new CarpetRecordWriter(recordConsumer, recordClass, carpetConfiguration);
+        }
+
+        void write(T record) {
+            recordConsumer.startMessage();
+            writer.write(record);
+            recordConsumer.endMessage();
+        }
+
+    }
+
 }
