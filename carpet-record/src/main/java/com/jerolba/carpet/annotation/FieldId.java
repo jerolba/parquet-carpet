f55de9f4095fdf453fa7137e335ba7e5f99303dc
André Rouél <andre.rouel@gmail.com>
2025-11-13 14:07:02 +0100

    Fix license header

--- carpet-record/src/main/java/com/jerolba/carpet/annotation/FieldId.java
+++ carpet-record/src/main/java/com/jerolba/carpet/annotation/FieldId.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2025 Jerónimo López Bezanilla
+ * Copyright 2023 Jerónimo López Bezanilla
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
