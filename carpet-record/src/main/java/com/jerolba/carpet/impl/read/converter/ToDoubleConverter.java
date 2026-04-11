52c2d32daf98804889b6b846f7bd3facea776646
jerolba <jerolba@gmail.com>
2026-04-11 15:23:42 +0000

    Change license Copyright year to 2026

--- carpet-record/src/main/java/com/jerolba/carpet/impl/read/converter/ToDoubleConverter.java
+++ carpet-record/src/main/java/com/jerolba/carpet/impl/read/converter/ToDoubleConverter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2023 Jerónimo López Bezanilla
+ * Copyright 2026 Jerónimo López Bezanilla
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
